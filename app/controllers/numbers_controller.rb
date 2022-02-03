55c57c4431badd9c7da94d1244f21ebc673424a1
divyakesharwani <dkesharw@chicagobooth.edu>
2022-02-03 04:35:54 +0000

    part 1 and part 2 done.

--- app/controllers/numbers_controller.rb
+++ app/controllers/numbers_controller.rb
@@ -10,4 +10,16 @@ class NumbersController < ApplicationController
 
     render({ :template => "lottery_stuff/woohoo.html.erb"})
   end 
+
+  def looser
+    @monkey = Array.new
+
+    5.times do
+      donkey = rand(1...100)
+
+      @monkey.push(donkey)
+    end
+    render({:template => "lottery_stuff/poohoo.html.erb"})
+  end
 end
+ 
